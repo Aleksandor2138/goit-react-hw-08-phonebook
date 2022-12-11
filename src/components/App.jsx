62265cb5cb46d8d7e9baae1ecade5badb9c3975a
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-12-12 00:29:35 +0200

    правки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, lazy } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAll } from 'redux/contacts/requestAPI';
-import { selectIsLoading, selectError } from 'redux/contacts/contactsSlise';
+import React, {  lazy } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchAll } from 'redux/contacts/requestAPI';
+// import { selectIsLoading, selectError } from 'redux/contacts/contactsSlise';
 import { Route, Routes } from 'react-router-dom';
 import { HeaderAll } from './Header/Header';
 
@@ -12,13 +12,6 @@ const SignIn = lazy(() => import('../pages/SignIn'));
 const PhoneBook = lazy(() => import('../pages/Phonebook'));
 
 const App = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectIsLoading);
-  // const error = useSelector(selectError);
-
-  // useEffect(() => {
-  //   dispatch(fetchAll());
-  // }, [dispatch]);
 
   return (
     <Routes>
@@ -27,28 +20,9 @@ const App = () => {
         <Route path="sign-up" element={<SignUp />} />
         <Route path="sign-in" element={<SignIn />} />
         <Route path="phonebook" element={<PhoneBook />} />
-        {/* <Route path="movies/:id" element={<MoviesDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route> */}
         <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
-    // <Section>
-    //   <Containet>
-    //     <div>
-    //       <H1>Phonebook</H1>
-    //       <ContactForm />
-    //     </div>
-
-    //     <DivList>
-    //       <h2>Contacts</h2>
-    //       <Filter />
-    //       {isLoading && !error && <Loader />}
-    //       <ContactList />
-    //     </DivList>
-    //   </Containet>
-    // </Section>
   );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+
+
 import App from 'components/App';
 import './index.css';
 
